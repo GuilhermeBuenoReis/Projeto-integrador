fffc80ffa753a32b0661c011c6a8ecf7af7615c4
GuilhermeBuenoReis <149843151+GuilhermeBuenoReis@users.noreply.github.com>
2023-11-11 16:16:25 -0300

    commit-scrrenRegister

---
+++ integrador-app/pages/register.tsx
@@ -0,0 +1,12 @@
+
+
+function register() {
+  return (
+
+        <main className="bg-[url('../public/background-register.jpg')] w-screen h-screen flex items-center justify-center" >
+            <h1>Olá</h1>
+        </main>
+  );
+}
+
+export default register;
